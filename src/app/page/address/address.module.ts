a455207fe6b3b93a04d68208f23876b39ea65c85
canguruhh <sven@mutzl.org>
2021-03-17 11:21:35 +0100

    show address details

--- src/app/page/address/address.module.ts
+++ src/app/page/address/address.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { AddressRoutingModule } from './address-routing.module';
 import { AddressComponent } from './address.component';
 import { MatTableModule } from '@angular/material/table'
+import { MatCardModule } from '@angular/material/card'
+import { MatDividerModule } from '@angular/material/divider'
+import { FlexLayoutModule } from '@angular/flex-layout'
 
 @NgModule({
   declarations: [AddressComponent],
@@ -10,6 +13,9 @@ import { MatTableModule } from '@angular/material/table'
     CommonModule,
     AddressRoutingModule,
     MatTableModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatDividerModule,
   ]
 })
 export class AddressModule { }
