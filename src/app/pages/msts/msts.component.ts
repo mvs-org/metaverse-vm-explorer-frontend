96afa10a49d36abd4c250f0e2c90ea4aeed7e96b
canguruhh <sven@mutzl.org>
2021-06-05 13:34:02 +0200

    add genefinance pair data to msts page

--- src/app/pages/msts/msts.component.ts
+++ src/app/pages/msts/msts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 import { Apollo } from 'apollo-angular'
 import gql from 'graphql-tag'
 
@@ -13,26 +13,69 @@ export class MSTsComponent implements OnInit {
   msts: any[]
   error: any
 
+  pairs: any[]
+
   constructor(private apollo: Apollo) { }
 
   async ngOnInit() {
+    this.loadMSTs()
+    this.loadPairs()
+  }
+
+  async loadMSTs(){
     const { data, loading, error } = await this.apollo
+    .query<any>({
+      query: gql`
+      {
+        msts {
+          symbol
+          name
+          decimals
+          logoURI
+          address
+        }
+      }
+    `,
+    }).toPromise()
+  this.msts = data?.msts
+  this.loading = loading
+  this.error = error
+  }
+
+  async loadPairs() {
+    const { data, loading, error } = await this.apollo.use('genefinance')
       .query<any>({
         query: gql`
-        {
-          msts {
-            symbol
-            name
-            decimals
-            logoURI
-            address
-          }
-        }
-      `,
-    }).toPromise()
-    this.msts = data?.msts
-    this.loading = loading
-    this.error = error
+{
+  pairs(where: {volumeUSD_gt:0}){
+    token0{
+      symbol
+    }
+    token1{
+      symbol
+    }
+    token0Price
+    token1Price
+    volumeToken0
+    volumeToken1
+    volumeUSD
+    pairHourData(orderBy:hourStartUnix orderDirection:desc first: 24){
+      hourStartUnix
+      reserveUSD
+      hourlyVolumeUSD
+      hourlyTxns
+      hourlyVolumeToken0
+      hourlyVolumeToken1
+    }
+  }
+}
+      `
+      }).toPromise()
+    this.pairs = data?.pairs.map((pair:any)=>({
+      ...pair,
+      volumeUSD24h: pair.pairHourData.reduce((acc, cur)=>acc+Number(cur.hourlyVolumeUSD), 0),
+      txs24h: pair.pairHourData.reduce((acc, cur)=>acc+Number(cur.hourlyTxns), 0),
+    }))
   }
 
 }
