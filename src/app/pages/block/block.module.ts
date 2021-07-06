5dcbf700bf54f60e55a411064c60b41c43a7bd39
Laurent Salou <laurent.salou@hotmail.fr>
2021-07-06 19:28:44 +0200

    Block page translation

--- src/app/pages/block/block.module.ts
+++ src/app/pages/block/block.module.ts
@@ -4,6 +4,7 @@ import { BlockRoutingModule, routedComponents } from './block-routing.module'
 import { NbCardModule, NbListModule, NbIconModule } from '@nebular/theme'
 import { TimerModule } from '../components/timer/timer.module'
 import { PipesModule } from '../../pipes/pipes.module'
+import { TranslateModule } from '@ngx-translate/core'
 
 @NgModule({
   imports: [
@@ -14,6 +15,7 @@ import { PipesModule } from '../../pipes/pipes.module'
     TimerModule,
     PipesModule,
     BlockRoutingModule,
+    TranslateModule.forChild(),
   ],
   declarations: [
     ...routedComponents,
