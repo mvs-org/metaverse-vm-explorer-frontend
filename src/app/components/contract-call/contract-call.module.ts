fb85ccec64fa1434b33b1d89416236ca1cccf0e5
Laurent Salou <laurent.salou@hotmail.fr>
2021-07-08 16:27:50 +0200

    Components translation

--- src/app/components/contract-call/contract-call.module.ts
+++ src/app/components/contract-call/contract-call.module.ts
@@ -3,8 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ContractCallComponent } from './contract-call.component';
 import { FormsModule } from '@angular/forms'
 import { NbButtonModule } from '@nebular/theme'
-
-
+import { TranslateModule } from '@ngx-translate/core'
 
 @NgModule({
   declarations: [
@@ -14,6 +13,7 @@ import { NbButtonModule } from '@nebular/theme'
     CommonModule,
     FormsModule,
     NbButtonModule,
+    TranslateModule.forChild(),
   ],
   exports: [
     ContractCallComponent,
