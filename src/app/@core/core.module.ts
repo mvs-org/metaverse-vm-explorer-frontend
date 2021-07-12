cd7ae4c3fd3cfa8e05ca72a9abad11fe8ec92aec
canguruhh <sven@mutzl.org>
2021-07-12 08:45:47 +0200

    cleanup core module

--- src/app/@core/core.module.ts
+++ src/app/@core/core.module.ts
@@ -1,77 +1,21 @@
-import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MAT_RIPPLE_GLOBAL_OPTIONS } from '@angular/material/core';
-import { NbAuthModule, NbDummyAuthStrategy } from '@nebular/auth';
-import { NbSecurityModule, NbRoleProvider } from '@nebular/security';
-import { of as observableOf } from 'rxjs';
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { NbAuthModule } from '@nebular/auth'
 
-import { throwIfAlreadyLoaded } from './module-import-guard';
+import { throwIfAlreadyLoaded } from './module-import-guard'
 import {
   AnalyticsService,
   LayoutService,
   SeoService,
   StateService,
-} from './utils';
-import { RippleService } from './utils/ripple.service';
-import { MockDataModule } from './mock/mock-data.module';
-
-const socialLinks = [];
-
-const DATA_SERVICES = [
-  {provide: MAT_RIPPLE_GLOBAL_OPTIONS, useExisting: RippleService},
-];
-
-export class NbSimpleRoleProvider extends NbRoleProvider {
-  getRole() {
-    // here you could provide any role based on any auth flow
-    return observableOf('guest');
-  }
-}
+} from './utils'
 
 export const NB_CORE_PROVIDERS = [
-  ...MockDataModule.forRoot().providers,
-  ...DATA_SERVICES,
-  ...NbAuthModule.forRoot({
-
-    strategies: [
-      NbDummyAuthStrategy.setup({
-        name: 'email',
-        delay: 3000,
-      }),
-    ],
-    forms: {
-      login: {
-        socialLinks: socialLinks,
-      },
-      register: {
-        socialLinks: socialLinks,
-      },
-    },
-  }).providers,
-
-  NbSecurityModule.forRoot({
-    accessControl: {
-      guest: {
-        view: '*',
-      },
-      user: {
-        parent: 'guest',
-        create: '*',
-        edit: '*',
-        remove: '*',
-      },
-    },
-  }).providers,
-
-  {
-    provide: NbRoleProvider, useClass: NbSimpleRoleProvider,
-  },
   AnalyticsService,
   LayoutService,
   SeoService,
   StateService,
-];
-
+]
 @NgModule({
   imports: [
     CommonModule,
@@ -83,7 +27,7 @@ export const NB_CORE_PROVIDERS = [
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
-    throwIfAlreadyLoaded(parentModule, 'CoreModule');
+    throwIfAlreadyLoaded(parentModule, 'CoreModule')
   }
 
   static forRoot(): ModuleWithProviders<CoreModule> {
@@ -92,6 +36,6 @@ export class CoreModule {
       providers: [
         ...NB_CORE_PROVIDERS,
       ],
-    };
+    }
   }
 }
