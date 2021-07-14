520160703c23e5c23142127c2d82e6e94a0ae2e1
canguruhh <sven@mutzl.org>
2021-07-14 14:21:13 +0200

    copyright notice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 /**
  * @license
  * Copyright Akveo. All Rights Reserved.
+ * Copyright Metaverse Foundation. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 import { Component, OnInit } from '@angular/core'
