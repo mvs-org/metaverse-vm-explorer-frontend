510bf2e8e30deb2d63579a41b06924446219c734
Laurent Salou <laurent.salou@hotmail.fr>
2021-07-14 18:01:12 +0200

    Translate timer to chinese

--- src/app/pages/components/timer/timer.module.ts
+++ src/app/pages/components/timer/timer.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { TimerComponent } from './timer.component'
+import { TranslateModule } from '@ngx-translate/core'
 
 @NgModule({
   imports: [
     CommonModule,
+    TranslateModule.forChild(),
   ],
   declarations: [
     TimerComponent,
