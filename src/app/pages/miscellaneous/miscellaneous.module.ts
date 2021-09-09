7407f588f197df671998b0497f9abccdaaba6c1d
Laurent Salou <laurent.salou@hotmail.fr>
2021-09-09 18:36:56 +0200

    Translate 404 not found page

--- src/app/pages/miscellaneous/miscellaneous.module.ts
+++ src/app/pages/miscellaneous/miscellaneous.module.ts
@@ -5,12 +5,14 @@ import { ThemeModule } from '../../@theme/theme.module'
 import { MiscellaneousRoutingModule } from './miscellaneous-routing.module'
 import { MiscellaneousComponent } from './miscellaneous.component'
 import { NotFoundComponent } from './not-found/not-found.component'
+import { TranslateModule } from '@ngx-translate/core'
 
 @NgModule({
   imports: [
     ThemeModule,
     NbCardModule,
     NbButtonModule,
+    TranslateModule.forChild(),
     MiscellaneousRoutingModule,
   ],
   declarations: [
