d1af9483caa229048f83da2996ddfc49010fb3e2
Laurent Salou <laurent.salou@hotmail.fr>
2021-11-30 16:35:43 +0100

    Update prod config with new parameters

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,10 @@
  */
 export const environment = {
   production: true,
+  showTrendingDapps: true,
+  showGenefinanceData: true,
+  showMits: true,
+  name: "<b>MetaverseVM</b> Explorer",
   graphqlEndpoint: 'https://vm-explorer.mvs.org/ql/',
   rpc_url: 'https://vm.mvs.org/mainnet/9434aa75-4d76-43fc-92cf-ff45d4cabcda',
 };
